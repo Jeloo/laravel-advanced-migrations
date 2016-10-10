ca4e0279f0560cdbbe76876e5d9d95575d1f3fed
Jeloo <i.v.yaroslavtsev@gmail.com>
2016-10-10 22:00:31 +0300

    Create migration name parser

---
+++ src/MigrationNameParser.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace Jeloo\LaraMigrations;
+
+/**
+ * Parse migration name in order to get verb, table name and some migration schema details
+ * Class MigrationNameParser
+ * @package Jeloo\LaraMigrations
+ */
+class MigrationNameParser
+{
+    /**
+     * @var string
+     */
+    private $migrationName;
+
+    /**
+     * @var array
+     */
+    private $allowedVerbs = [
+        'create',
+        'add',
+        'drop',
+    ];
+
+    /**
+     * @var array
+     */
+    private $tableNameDelimiters = ['__', '_table'];
+
+    const DELIMITER = '_';
+
+    public function __construct($migrationName)
+    {
+        $this->migrationName = $migrationName;
+    }
+
+    /**
+     * @return string
+     * @throws \InvalidArgumentException
+     */
+    public function parseTableName()
+    {
+        if (! array_key_exists(1, explode(self::DELIMITER, $this->migrationName))) {
+            throw new \InvalidArgumentException('Migration name is invalid: second word must represent table name');
+        }
+
+        $start = strpos($this->migrationName, self::DELIMITER) + 1;
+        $end = $this->getLastTableNamePos() - $start;
+
+        return substr(
+            $this->migrationName,
+            $start,
+            $end
+        );
+    }
+
+    /**
+     * @return string
+     * @throws \InvalidArgumentException
+     */
+    public function parseVerb()
+    {
+        $verb = substr($this->migrationName, 0, strpos($this->migrationName, self::DELIMITER));
+
+        if (! in_array($verb, $this->allowedVerbs)) {
+            throw new \InvalidArgumentException(
+                sprintf('Invalid verb: %s. Allowed verbs: %s', $verb, implode(', ', $this->allowedVerbs))
+            );
+        }
+
+        return $verb;
+    }
+
+    /**
+     * Get string right position of the table name
+     *
+     * @return int
+     */
+    final private function getLastTableNamePos()
+    {
+        foreach ($this->tableNameDelimiters as $delimiter) {
+            $end = strpos($this->migrationName, $delimiter);
+            if ($end !== false) {
+                return $end;
+            }
+        }
+
+        return strlen($this->migrationName);
+    }
+
+}
--- tests/MigrationNameParserTest.php
+++ tests/MigrationNameParserTest.php
@@ -1,12 +1,34 @@
 <?php
 
-/**
- * Created by PhpStorm.
- * User: jeloo
- * Date: 10.10.16
- * Time: 18:40
- */
-class MigrationNameParserTest
+use Jeloo\LaraMigrations\MigrationNameParser as Parser;
+use PHPUnit_Framework_TestCase as TestCase;
+
+class MigrationNameParserTest extends TestCase
 {
+
+    public function testParseVerb()
+    {
+        $parser = new Parser('create_users_table');
+        $this->assertEquals($parser->parseVerb(), 'create');
+
+        $this->expectException(\InvalidArgumentException::class);
+        $this->expectExceptionMessage('Invalid verb: foo. Allowed verbs: create, add, drop');
+
+        $parser = new Parser('foo_users_table');
+        $parser->parseVerb();
+    }
+
+    public function testParseTableName()
+    {
+        $parser = new Parser('create_users_table');
+        $this->assertEquals($parser->parseTableName(), 'users');
+
+        $parser = new Parser('drop_users');
+        $this->assertEquals($parser->parseTableName(), 'users');
+
+        $this->expectException(\InvalidArgumentException::class);
+        $parser = new Parser('drop');
+        $parser->parseTableName();
+    }
 
 }
